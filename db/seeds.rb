8908b3aa3fe1e3a72f07f6141073ef35bb5f602e
Fateh Ullah Mazhar <eng.fatehullah41@gmail.com>
2020-10-12 14:01:47 +0500

    create model of users & messages and thier migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Fateh", password: "password")
+User.create(username: "Zain", password: "password1")
+User.create(username: "Haroon", password: "password2")
+User.create(username: "Farhan", password: "password3")
+User.create(username: "Moiz", password: "password4")
+User.create(username: "Uzair", password: "password5")
+User.create(username: "Usman", password: "password6")
+
