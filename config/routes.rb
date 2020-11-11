ca757a0a33fc302682956aaac43fe424e157fa6d
Fateh Ullah Mazhar <eng.fatehullah41@gmail.com>
2020-11-11 12:05:30 +0500

    Create login page & chatroom page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   get 'login', to: 'session#new'
   root  'chatroom#index'
 
-  resources :users
-  resources :messages
+  #resources :users
+  #resources :messages
+
+  post 'login', to: 'session#create'
+  delete 'logout', to: 'session#destroy'
+  post 'message', to: 'message#create '
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
